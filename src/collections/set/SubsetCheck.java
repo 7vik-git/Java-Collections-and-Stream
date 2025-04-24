3ddbf270864bec034bd1d9b5192fb3c252289688
7vik-git <sathviklingamsetti@gmail.com>
2025-04-24 12:34:56 +0530

    Collections Initial Commit

---
+++ src/collections/set/SubsetCheck.java
@@ -0,0 +1,18 @@
+package collections.set;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SubsetCheck {
+    public static void main(String[] args) {
+        // Example sets
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+
+        // Check if set1 is a subset of set2
+        boolean isSubset = set2.containsAll(set1);
+
+        System.out.println("Is Set1 a subset of Set2? " + isSubset);
+    }
+}
---
+++ src/collections/set/SymmetricDifference.java
@@ -0,0 +1,9 @@
+package collections.set;
+
+import java.util.HashSet;
+
+public class SymmetricDifference {
+    HashSet<Integer> hset = new HashSet<>();
+    HashSet<Integer> hset1 = new HashSet<>();
+    HashSet<Integer> res = new HashSet<>();
+}
