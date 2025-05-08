e02ac6134647996e3d374eec94f57d03a78e109b
7vik-git <sathviklingamsetti@gmail.com>
2025-05-08 12:00:00 +0530

    Initial implementation for Exception Handling module

---
+++ src/exceptionhandling/bank/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package exceptionhandling.bank;
+
+public class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message){
+        super(message);
+    }
+}
